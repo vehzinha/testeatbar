2c78add1282255b10418559d464a8a1e7b630c73
vehzinha <101737211+vehzinha@users.noreply.github.com>
2024-11-14 12:08:50 -0300

    Update and rename index.html to barra-acessibilidade.php

---
+++ barra-acessibilidade.php
@@ -0,0 +1,9 @@
+<!-- Barra de Acessibilidade -->
+<div id="accessibility-bar">
+    <button onclick="increaseFontSize()">A+</button>
+    <button onclick="decreaseFontSize()">A-</button>
+    <button onclick="resetFontSize()">A</button>
+    <button onclick="toggleHighContrast()">Alto contraste</button>
+    <button onclick="toggleLowContrast()">Baixo contraste</button>
+    <button onclick="toggleFont()">Mudar Fonte</button>
+</div>
